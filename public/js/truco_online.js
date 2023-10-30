c424bd1e2bccea4d41a3f2d752816bf81e48ffc4
felipe <fgagliardi@pioix.edu.ar>
2023-10-30 10:22:36 -0300

    INICIO API

--- public/js/truco_online.js
+++ public/js/truco_online.js
@@ -1,6 +1,23 @@
-/** ACCESS_KEY = 6bb49a87900f6c4e8928b14b2a9ec162
+/** ACCESS_KEY = 17e37d155baf026cabad4d2fe2ab0912
  * 
  * https://weatherstack.com/documentation
  * 
  * 
  */
+
+
+/*const axios = require('axios');
+const params = {
+  access_key: '17e37d155baf026cabad4d2fe2ab0912',
+  query: 'Buenos Aires'
+}
+
+axios.get('https://api.weatherstack.com/current', {params})
+  .then(response => {
+    const apiResponse = response.data;
+    console.log(`Current temperature in ${apiResponse.location.name} is ${apiResponse.current.temperature}℃`);
+  }).catch(error => {
+    console.log(error);
+  });
+
+*/
